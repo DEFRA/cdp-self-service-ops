f2b8ac9afd4ea34eac4e2a2ce921a9cad33d0b33
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2023-06-02 16:14:34 +0100

    Auto-detects cluster when deploying a service

--- src/api/v1/deploy/helpers/deploy-service-payload-validation.js
+++ src/api/v1/deploy/helpers/deploy-service-payload-validation.js
@@ -1,14 +1,10 @@
 import Joi from 'joi'
 
 function deployServicePayloadSchema() {
-  const clusters = ['frontend', 'backend']
   return Joi.object({
     imageName: Joi.string().min(1).required(),
     version: Joi.string()
       .pattern(/^v?\d+\.\d+\.\d+$/)
-      .required(),
-    cluster: Joi.string()
-      .valid(...clusters)
       .required()
   })
 }
