58c6a2ac4214d8fa3c7820e62bc492176329217a
Ben Chidgey <ben@feedmypixel.com>
2023-10-04 09:08:54 +0100

    Add dev and test environments

--- src/config/ready-environments.js
+++ src/config/ready-environments.js
@@ -1,4 +1,4 @@
 // TODO remove once other envs have been set up
-const readyEnvironments = ['management', 'infra-dev']
+const readyEnvironments = ['management', 'infra-dev', 'dev', 'test']
 
 export { readyEnvironments }
