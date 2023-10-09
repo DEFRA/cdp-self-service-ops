4f71b5839c520e16e886df2bfdcac85458d90182
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2023-10-09 11:53:42 +0100

    debug logging

--- src/api/deploy/controllers/deploy-service.js
+++ src/api/deploy/controllers/deploy-service.js
@@ -1,6 +1,7 @@
 import { deployServiceValidation } from '~/src/api/deploy/helpers/schema/deploy-service-validation'
 import { createDeploymentPullRequest } from '~/src/api/deploy/helpers/create-deployment-pull-request'
 import { authStrategy } from '~/src/helpers/auth-stratergy'
+import { createLogger } from '~/src/helpers/logger'
 
 const deployServiceController = {
   options: {
@@ -15,9 +16,10 @@ const deployServiceController = {
     }
   },
   handler: async (request, h) => {
+    const logger = createLogger()
     const payload = request.payload
     payload.user = request.auth.credentials?.profile?.displayName ?? 'unknown'
-
+    logger.info(request.auth)
     await createDeploymentPullRequest(payload)
 
     return h.response({ message: 'success' }).code(200)
