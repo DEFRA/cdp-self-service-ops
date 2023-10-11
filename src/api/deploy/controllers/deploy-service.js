d730f095cfbe1bfd2e975961293dcd01238cfbdc
Ben Chidgey <ben@feedmypixel.com>
2023-10-11 13:42:50 +0100

    Remove debug logging

--- src/api/deploy/controllers/deploy-service.js
+++ src/api/deploy/controllers/deploy-service.js
@@ -16,21 +16,8 @@ const deployServiceController = {
     }
   },
   handler: async (request, h) => {
-    request.logger.info(
-      'SR - Request payload is: %s',
-      JSON.stringify(request.payload)
-    )
-    request.logger.info('SR - Auth is: %s', JSON.stringify(request.auth))
-    request.logger.info(
-      'SR - User is: %s',
-      request.auth?.credentials?.displayName
-    )
-    request.logger.info('SR - Testing request logger')
-
     const payload = request.payload
     payload.user = request.auth?.credentials?.displayName ?? 'n/a'
-
-    request.logger.info('SR - Updated Payload is: %s', JSON.stringify(payload)) // Works
 
     await registerDeployment(payload)
     await createDeploymentPullRequest(payload)
