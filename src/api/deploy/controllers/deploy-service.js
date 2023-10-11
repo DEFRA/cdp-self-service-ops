be2015ff64a907402176133df0defef5dde3d266
Ben Chidgey <ben@feedmypixel.com>
2023-10-11 11:54:45 +0100

    Logging part 4

--- src/api/deploy/controllers/deploy-service.js
+++ src/api/deploy/controllers/deploy-service.js
@@ -2,6 +2,7 @@ import { deployServiceValidation } from '~/src/api/deploy/helpers/schema/deploy-
 import { createDeploymentPullRequest } from '~/src/api/deploy/helpers/create-deployment-pull-request'
 import { authStrategy } from '~/src/helpers/auth-stratergy'
 import { registerDeployment } from '~/src/api/deploy/helpers/register-deployment'
+import { createLogger } from '~/src/helpers/logger'
 
 const deployServiceController = {
   options: {
@@ -16,14 +17,26 @@ const deployServiceController = {
     }
   },
   handler: async (request, h) => {
+    const logger = createLogger()
+
     request.logger.info(request.payload, 'Request payload is:')
-    // TODO once issue has been fixed remove logging exposing auth contents
     request.logger.info(request.auth, 'Authed user is:')
+    request.logger.info('Testing request logger')
+
+    request.logger.info('SR - Request payload is: %s', request.payload)
+    request.logger.info('SR - Authed user is: %s', request.auth)
+    request.logger.info('SR - Testing request logger')
+
+    logger.info(request.payload, 'L - Request payload is:')
+    logger.info(request.auth, 'L - Authed user is:')
+    logger.info('L - Testing request logger')
 
     const payload = request.payload
     payload.user = request.auth?.credentials?.displayName ?? 'n/a'
 
     request.logger.info(payload, 'Updated Payload is:')
+    request.logger.info('SR - Updated Payload is: %s', JSON.stringify(payload))
+    logger.info(payload, 'L - Updated Payload is:')
 
     await registerDeployment(payload)
     await createDeploymentPullRequest(payload)
