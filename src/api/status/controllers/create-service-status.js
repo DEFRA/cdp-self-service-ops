7d406a7fe9c9e604b340f774f7e263ebc73ded6e
Ben Chidgey <ben@feedmypixel.com>
2023-10-27 08:54:12 +0100

    Merge in status work from bens branch
    - Align file naming with rest of codebase
    - Align controllers location with rest of code base
    - Bring in work from bens branch
    - Update url endpoint paths

---
+++ src/api/status/controllers/create-service-status.js
@@ -0,0 +1,29 @@
+import Joi from 'joi'
+import Boom from '@hapi/boom'
+
+import { getRepositoryStatus } from '~/src/api/status/helpers/get-repository-status'
+
+const createServiceStatusController = {
+  options: {
+    validate: {
+      params: Joi.object({
+        repositoryName: Joi.string()
+      })
+    }
+  },
+  handler: async (request, h) => {
+    const repositoryName = request.params.repositoryName
+    const repositoryStatus = await getRepositoryStatus(
+      request.db,
+      repositoryName
+    )
+
+    if (!repositoryStatus) {
+      return Boom.notFound(`Service ${repositoryName} not found`)
+    }
+
+    return h.response({ message: 'Success', status: repositoryStatus })
+  }
+}
+
+export { createServiceStatusController }
