ee9dd27e30234ba686cae6db943020e6794083cc
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2023-10-31 08:56:09 +0000

    fixes placeholder api call params

--- src/listeners/github/helpers/createPlaceholderArtifact.js
+++ src/listeners/github/helpers/createPlaceholderArtifact.js
@@ -1,10 +1,14 @@
 import { config } from '~/src/config'
 import qs from 'qs'
+import { createLogger } from '~/src/helpers/logging/logger'
 
 async function createPlaceholderArtifact({ service, githubUrl }) {
+  const logger = createLogger()
+
   const queryString = qs.stringify(
     {
-      ...(service && { githubUrl })
+      service,
+      githubUrl
     },
     { addQueryPrefix: true }
   )
@@ -12,6 +16,8 @@ async function createPlaceholderArtifact({ service, githubUrl }) {
   const url = `${config.get(
     'portalBackendApiUrl'
   )}/artifacts/placeholder${queryString}`
+
+  logger.info(`calling create placeholder with ${service} ${githubUrl}`)
   return await fetch(url, {
     method: 'post'
   })
