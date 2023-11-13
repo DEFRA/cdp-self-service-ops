30623edd49c556f18c1cdb58730231306ce03cd9
Anargyros Akrivos <argy@akrivos.me>
2023-11-13 11:32:21 +0200

    Auth checks for deploying a service

--- src/api/deploy/helpers/generate-deploy-message.js
+++ src/api/deploy/helpers/generate-deploy-message.js
@@ -1,52 +1,56 @@
 import { lookupTenantService } from '~/src/api/deploy/helpers/lookup-tenant-service'
 import { serviceToSecretsMap } from '~/src/api/deploy/helpers/service-to-secrets-map'
 
-async function generateDeployMessage(payload) {
-  const tenantService = await lookupTenantService(
-    payload.environment,
-    payload.imageName
-  )
+async function generateDeployMessage(
+  imageName,
+  version,
+  environment,
+  instanceCount,
+  cpu,
+  memory,
+  user
+) {
+  const tenantService = await lookupTenantService(environment, imageName)
 
   if (tenantService === undefined) {
-    throw new Error(`Unable to lookup ${payload.imageName} in tenant services`)
+    throw new Error(`Unable to lookup ${imageName} in tenant services`)
   }
 
   return {
-    container_image: payload.imageName,
+    container_image: imageName,
     container_port: 8085,
-    container_version: payload.version,
-    desired_count: payload.instanceCount,
+    container_version: version,
+    desired_count: instanceCount,
     env_files: [
       {
-        value: `arn:aws:s3:::cdp-${payload.environment}-service-configs/global/global_protected_fixed.env`,
+        value: `arn:aws:s3:::cdp-${environment}-service-configs/global/global_protected_fixed.env`,
         type: 's3'
       },
       {
-        value: `arn:aws:s3:::cdp-${payload.environment}-service-configs/services/${payload.imageName}/${payload.environment}/${payload.imageName}.env`,
+        value: `arn:aws:s3:::cdp-${environment}-service-configs/services/${imageName}/${environment}/${imageName}.env`,
         type: 's3'
       },
       {
-        value: `arn:aws:s3:::cdp-${payload.environment}-service-configs/services/${payload.imageName}/defaults.env`,
+        value: `arn:aws:s3:::cdp-${environment}-service-configs/services/${imageName}/defaults.env`,
         type: 's3'
       },
       {
-        value: `arn:aws:s3:::cdp-${payload.environment}-service-configs/environments/${payload.environment}/defaults.env`,
+        value: `arn:aws:s3:::cdp-${environment}-service-configs/environments/${environment}/defaults.env`,
         type: 's3'
       }
     ],
     env_vars: {},
-    healthcheck: `/${payload.imageName}/health`,
-    name: payload.imageName,
-    secrets:
-      serviceToSecretsMap[payload.imageName]?.[payload.environment] ?? {},
-    task_cpu: payload.cpu,
-    task_memory: payload.memory,
+    healthcheck: `/${imageName}/health`,
+    name: imageName,
+    secrets: serviceToSecretsMap[imageName]?.[environment] ?? {},
+    task_cpu: cpu,
+    task_memory: memory,
     deploy_metrics: true,
-    environment: payload.environment,
+    environment,
     zone: tenantService.zone,
     deployed_by: {
-      user_id: payload.user.id,
-      display_name: payload.user.displayName
+      user_id: user.id,
+      display_name: user.displayName
     }
   }
 }
