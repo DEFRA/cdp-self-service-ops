47999e0c6b762fa4a5de8d41f72dc7306e37da06
Ben Chidgey <ben@feedmypixel.com>
2023-11-22 10:49:38 +0000

    Use SQS consumer for the sqs listener

--- src/listeners/github/github-events-plugin.js
+++ src/listeners/github/github-events-plugin.js
@@ -1,11 +1,12 @@
-import { listen } from '~/src/listeners/github/sqs-listener'
+import { sqsListener } from '~/src/listeners/github/sqs-listener'
 
 const githubEventsPlugin = {
-  name: 'githubEventsPlugin',
-  version: '1.0.0',
-  register: async function (server) {
-    // start the listener but do not await its completion
-    listen(server)
+  plugin: {
+    name: 'githubEventsPlugin',
+    version: '1.0.0',
+    register: function (server) {
+      sqsListener(server)
+    }
   }
 }
 
