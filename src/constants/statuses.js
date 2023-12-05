aff4b9bd6771d3cd2ab2995be8dc726bc47880b6
Ben Chidgey <ben@feedmypixel.com>
2023-12-05 10:44:42 +0000

    Global helpers and constants

---
+++ src/constants/statuses.js
@@ -0,0 +1,11 @@
+const statuses = {
+  notRequested: 'not-requested',
+  requested: 'requested',
+  completed: 'completed',
+  raised: 'raised',
+  inProgress: 'in-progress',
+  success: 'success',
+  failure: 'failure'
+}
+
+export { statuses }
