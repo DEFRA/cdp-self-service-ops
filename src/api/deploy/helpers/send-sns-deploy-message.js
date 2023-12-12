1da0f3e603ee943f37107fa2a7ce875ae19cfa3b
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2023-12-12 15:32:36 +0000

    adds flexible fifo support

--- src/api/deploy/helpers/send-sns-deploy-message.js
+++ src/api/deploy/helpers/send-sns-deploy-message.js
@@ -1,8 +1,10 @@
 import { PublishCommand } from '@aws-sdk/client-sns'
 
 import { config } from '~/src/config'
+import * as crypto from 'crypto'
 
 async function sendSnsDeployMessage(snsClient, msg) {
+  const topic = config.get('snsDeployTopicArn')
   const input = {
     TopicArn: config.get('snsDeployTopicArn'),
     Message: JSON.stringify(msg, null, 2),
@@ -11,10 +13,16 @@ async function sendSnsDeployMessage(snsClient, msg) {
         DataType: 'String',
         StringValue: msg.environment
       }
-    },
-    MessageDeduplicationId: `${msg.environment}-${msg.container_image}-${msg.container_version}`,
-    MessageGroupId: msg.environment
+    }
   }
+
+  // At the time of writing localstack doesnt support fifo queues and will fail if you set these values on a non-fifo
+  // queue. Luckily, AWS requires all fifo queues to end with `.fifo` so we can selectively add these params.
+  if (topic.endsWith('fifo')) {
+    input.MessageDeduplicationId = crypto.randomUUID()
+    input.MessageGroupId = msg.environment
+  }
+
   const command = new PublishCommand(input)
   return await snsClient.send(command)
 }
