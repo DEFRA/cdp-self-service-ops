f14b34804d09a1461c4e23f4c3df192edec45d51
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-02-01 15:45:52 +0000

    proxy fetch helper

--- src/helpers/oktokit.js
+++ src/helpers/oktokit.js
@@ -4,20 +4,9 @@ import { createAppAuth } from '@octokit/auth-app'
 import { config } from '~/src/config'
 import { restEndpointMethods } from '@octokit/plugin-rest-endpoint-methods'
 import { createPullRequest } from 'octokit-plugin-create-pull-request'
-import { ProxyAgent, fetch as undiciFetch } from 'undici'
+import { proxyFetch } from '~/src/helpers/proxy-fetch'
 
 const OctokitExtra = Octokit.plugin(restEndpointMethods, createPullRequest)
-
-const myFetch = (url, opts) => {
-  return undiciFetch(url, {
-    ...opts,
-    dispatcher: new ProxyAgent({
-      uri: config.get('httpsProxy'),
-      keepAliveTimeout: 10,
-      keepAliveMaxTimeout: 10
-    })
-  })
-}
 
 const init = () => {
   let cfg = {
@@ -27,7 +16,7 @@ const init = () => {
       privateKey: Buffer.from(config.get('gitHubAppPrivateKey'), 'base64'),
       installationId: config.get('gitHubAppInstallationId')
     },
-    request: { fetch: myFetch }
+    request: { fetch: proxyFetch }
   }
 
   // Test Mode, for use with cdp-portal-stubs
