bfc4719292e702b8b67656200bdf3e5b5acb7169
Ben Chidgey <ben@feedmypixel.com>
2024-02-22 10:16:32 +0000

    Add forms-designer missing secrets

--- src/api/deploy/helpers/service-to-secrets-map.js
+++ src/api/deploy/helpers/service-to-secrets-map.js
@@ -69,17 +69,22 @@ const serviceToSecretsMap = {
     dev: {
       SESSION_COOKIE_PASSWORD:
         'cdp/services/forms-designer:SESSION_COOKIE_PASSWORD',
+      REDIS_USERNAME: 'cdp/services/forms-designer:REDIS_USERNAME',
       REDIS_PASSWORD: 'cdp/services/forms-designer:REDIS_PASSWORD',
       SQUID_PASSWORD: 'cdp/services/forms-designer:SQUID_PASSWORD'
     },
     test: {
-      REDIS_PASSWORD: 'cdp/services/forms-designer:REDIS_PASSWORD'
+      REDIS_USERNAME: 'cdp/services/forms-designer:REDIS_USERNAME',
+      REDIS_PASSWORD: 'cdp/services/forms-designer:REDIS_PASSWORD',
+      SQUID_PASSWORD: 'cdp/services/forms-designer:SQUID_PASSWORD'
     },
     'perf-test': {
+      REDIS_USERNAME: 'cdp/services/forms-designer:REDIS_USERNAME',
       REDIS_PASSWORD: 'cdp/services/forms-designer:REDIS_PASSWORD',
       SQUID_PASSWORD: 'cdp/services/forms-designer:SQUID_PASSWORD'
     },
     prod: {
+      REDIS_USERNAME: 'cdp/services/forms-designer:REDIS_USERNAME',
       REDIS_PASSWORD: 'cdp/services/forms-designer:REDIS_PASSWORD',
       SQUID_PASSWORD: 'cdp/services/forms-designer:SQUID_PASSWORD'
     }
