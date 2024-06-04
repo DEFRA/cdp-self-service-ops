2ec06d7f63e7ee2c4acecf7fc169946b5067a61f
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-06-04 15:45:48 +0100

    384243 mongolock (#208)
    * Proxy may be http
    
    * Bumped dependencies
    
    * Added mongo-locks dep
    
    * Added mongo-locks helpers

--- src/helpers/mongodb.js
+++ src/helpers/mongodb.js
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb'
+import { LockManager } from 'mongo-locks'
 
 import { config } from '~/src/config'
 
@@ -19,12 +20,16 @@ const mongoPlugin = {
 
     const client = await MongoClient.connect(mongoUrl, mongoOptions)
     const db = client.db(databaseName)
+    const locker = new LockManager(db.collection('mongo-locks'))
+
     await createIndexes(db)
 
     server.logger.info(`mongodb connected to ${databaseName}`)
     server.decorate('server', 'mongoClient', client)
     server.decorate('server', 'db', db)
     server.decorate('request', 'db', db)
+    server.decorate('server', 'locker', locker)
+    server.decorate('request', 'locker', locker)
   }
 }
 
@@ -33,6 +38,7 @@ const createIndexes = async (db) => {
     .collection('status')
     .createIndex({ repositoryName: 1 }, { unique: true })
   await db.collection('status').createIndex({ repositoryName: 1, status: 1 })
+  await db.collection('mongo-locks').createIndex({ id: 1 })
 }
 
 export { mongoPlugin }
