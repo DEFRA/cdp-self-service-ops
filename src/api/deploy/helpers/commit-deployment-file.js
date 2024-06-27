4ebe2ebab4a344269888f568a5662098cc07286d
chotai <chotai@users.noreply.github.com>
2024-06-27 18:27:23 +0100

    Add environment and zone to deployment file

--- src/api/deploy/helpers/commit-deployment-file.js
+++ src/api/deploy/helpers/commit-deployment-file.js
@@ -14,6 +14,7 @@ async function commitDeploymentFile(
   const deployment = generateDeployment(
     user,
     payload,
+    zone,
     deploymentId,
     configCommitSha,
     false
@@ -34,6 +35,7 @@ async function commitDeploymentFile(
 function generateDeployment(
   user,
   payload,
+  zone,
   deploymentId,
   commitSha,
   deploy = true
@@ -48,6 +50,10 @@ function generateDeployment(
       configuration: {
         commitSha
       }
+    },
+    cluster: {
+      environment: payload.environment,
+      zone
     },
     resources: {
       instanceCount: payload.instanceCount,
