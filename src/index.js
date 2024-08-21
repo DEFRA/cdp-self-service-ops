081d46cabbba0881758578ee43c5f1b4f7b09ce2
Ben Chidgey <ben@feedmypixel.com>
2024-08-21 09:05:55 +0100

    Move startServer error reporting to catch
    Note this can be replaced with a high level await when moved to esm

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import { createLogger } from '~/src/helpers/logging/logger'
-import { startServer } from '~/src/helpers/start-server'
 import process from 'node:process'
 
-startServer()
+import { createLogger } from '~/src/helpers/logging/logger'
+import { startServer } from '~/src/helpers/start-server'
+
+startServer().catch((error) => {
+  const logger = createLogger()
+  logger.info('Server failed to start :(')
+  logger.error(error)
+
+  process.exitCode = 1
+})
 
 process.on('unhandledRejection', (error) => {
   const logger = createLogger()
