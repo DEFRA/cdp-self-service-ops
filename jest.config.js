bddcfaf452ca88d3e088fbff71919501d167962b
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-11-18 20:48:41 +0000

    WIP

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+/**
+ * @type {Config}
+ */
+export default {
   rootDir: '.',
   verbose: true,
   resetModules: true,
@@ -6,13 +9,17 @@ module.exports = {
   silent: false,
   preset: '@shelf/jest-mongodb',
   testMatch: ['**/src/**/*.test.js'],
+  globalTeardown: '<rootDir>/.jest/teardown.js',
   reporters: ['default', ['github-actions', { silent: false }], 'summary'],
   collectCoverageFrom: ['src/**/*.js'],
   coveragePathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/.server',
     '<rootDir>/src/__fixtures__',
-    'index.js'
+    '<rootDir>/src/server/common/test-helpers'
   ],
   coverageDirectory: '<rootDir>/coverage'
 }
+/**
+ * @import { Config } from 'jest'
+ */
