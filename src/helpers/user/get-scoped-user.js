f6d239a81fbe50cc5c3e18f5c1ed372d327d9817
Phil Segal <phil@phisec.co.uk>
2025-01-23 14:26:05 +0000

    Tidy up scopes to rely on USBE scope data

--- src/helpers/user/get-scoped-user.js
+++ src/helpers/user/get-scoped-user.js
@@ -2,17 +2,17 @@ import Boom from '@hapi/boom'
 
 import { getRepoTeams } from '~/src/api/deploy/helpers/get-repo-teams.js'
 
-export async function getScopedUser(imageName, auth, targetScope) {
+export async function getScopedUser(imageName, auth) {
+  const credentials = auth?.credentials
   const user = {
-    id: auth?.credentials?.id,
-    displayName: auth?.credentials?.displayName
+    id: credentials?.id,
+    displayName: credentials?.displayName
   }
-  const scope = auth?.credentials?.scope
-  if (!scope) {
-    throw Boom.forbidden('No scope found')
-  }
-  const isAdmin = scope.includes(targetScope)
-  if (!isAdmin) {
+  if (!credentials.scopeFlags?.isAdmin) {
+    const scope = credentials?.scope
+    if (!scope) {
+      throw Boom.forbidden('No scope found')
+    }
     const repoTeams = await getRepoTeams(imageName)
     const isTeamMember = repoTeams.some((team) => scope.includes(team.teamId))
     if (!isTeamMember) {
