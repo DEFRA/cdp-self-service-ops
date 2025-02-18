8db995799249e634876b8d20e3aef44ab5dd8be9
Phil Segal <phil@phisec.co.uk>
2025-02-18 10:50:29 +0000

    506788: Fix currentEnvironment validation to allow local as field is for reference only

--- src/api/deploy/helpers/schema/deploy-service-validation.js
+++ src/api/deploy/helpers/schema/deploy-service-validation.js
@@ -1,15 +1,11 @@
 import Joi from 'joi'
 import {
   cpuValidation,
-  currentEnvironmentValidation,
-  deploymentIdValidation,
   environmentValidation,
   instanceCountValidation,
   memoryValidation,
   repositoryNameValidation,
-  userWithUserIdValidation,
-  versionValidation,
-  zoneValidation
+  versionValidation
 } from '~/src/api/helpers/schema/common-validations.js'
 
 const deployServiceValidation = Joi.object({
@@ -21,31 +17,4 @@ const deployServiceValidation = Joi.object({
   memory: memoryValidation
 })
 
-const deploymentValidation = Joi.object({
-  deploymentId: deploymentIdValidation,
-  deploy: Joi.boolean().required(),
-  service: Joi.object({
-    name: repositoryNameValidation,
-    image: repositoryNameValidation,
-    version: versionValidation,
-    configuration: Joi.object({
-      commitSha: Joi.string().required()
-    }),
-    serviceCode: Joi.string()
-  }),
-  cluster: Joi.object({
-    environment: environmentValidation,
-    zone: zoneValidation
-  }),
-  resources: Joi.object({
-    instanceCount: instanceCountValidation,
-    cpu: cpuValidation,
-    memory: memoryValidation
-  }),
-  metadata: Joi.object({
-    user: userWithUserIdValidation,
-    deploymentEnvironment: currentEnvironmentValidation
-  })
-})
-
-export { deployServiceValidation, deploymentValidation }
+export { deployServiceValidation }
