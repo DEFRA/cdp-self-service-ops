3d4352bf68bf8a6205d7326bcf12537a3d9544a6
Phil Segal <phil@phisec.co.uk>
2025-07-03 16:31:53 +0100

    CORE-696: Add test and fix bug with body not being populated

---
+++ src/api/deploy-test-suite/controllers/trigger-test-suite.test.js
@@ -0,0 +1,140 @@
+import { generateSignature } from '~/src/api/helpers/pre/validate-portal-backend-request.js'
+import { randomUUID } from 'node:crypto'
+import { runTestSuite } from '~/src/api/deploy-test-suite/helpers/run-test-suite.js'
+import * as Hapi from '@hapi/hapi'
+import { triggerTestSuiteController } from '~/src/api/deploy-test-suite/controllers/trigger-test-suite.js'
+
+jest.mock('src/config/config.js', () => {
+  const actual = jest.requireActual('src/config/config.js')
+  return {
+    ...actual,
+    config: {
+      ...actual.config,
+      get: (key) => {
+        if (key === 'portalBackendSharedSecret') return 'mocked-secret'
+        return actual.config.get(key)
+      }
+    }
+  }
+})
+
+jest.mock('src/api/deploy-test-suite/helpers/run-test-suite.js')
+
+describe('Test Trigger Test Suite', () => {
+  /** @type {import('@hapi/hapi').Server} */
+  let server
+
+  beforeAll(() => {
+    server = Hapi.server()
+    server.route([
+      {
+        method: 'POST',
+        path: '/trigger-test-suite',
+        ...triggerTestSuiteController
+      }
+    ])
+  })
+
+  afterAll(async () => {
+    if (server) {
+      await server?.stop()
+    }
+  })
+
+  const method = 'POST'
+  const url = '/trigger-test-suite'
+  const payload = {
+    imageName: 'test-image',
+    environment: 'infra-dev',
+    cpu: 4096,
+    memory: 8192,
+    user: { id: randomUUID(), displayName: 'Test User' },
+    deployment: {
+      deploymentId: randomUUID(),
+      service: 'test-image',
+      version: '0.1.0'
+    }
+  }
+
+  test('call to trigger-test-suite fails without signature headers', async () => {
+    const { statusCode, payload: responsePayload } = await server.inject({
+      method,
+      payload,
+      url
+    })
+
+    const { message } = JSON.parse(responsePayload)
+
+    expect(statusCode).toBe(401)
+    expect(message).toBe('Missing or invalid signature headers')
+  })
+
+  test('call to trigger-test-suite fails with an invalid signature', async () => {
+    const timestamp = Math.floor(Date.now() / 1000).toString()
+    const { statusCode, payload: responsePayload } = await server.inject({
+      method,
+      url,
+      payload,
+      headers: {
+        'x-signature': 'invalid-signature',
+        'x-timestamp': timestamp,
+        'x-signature-version': 'v1',
+        'content-type': 'application/json'
+      }
+    })
+    const { message } = JSON.parse(responsePayload)
+
+    expect(statusCode).toBe(401)
+    expect(message).toBe('Invalid signature')
+  })
+
+  test('call to trigger-test-suite fails with an old timestamp', async () => {
+    const timestamp300SecondsAgo = Math.floor(
+      new Date(Date.now() - 300 * 1000) / 1000
+    ).toString()
+    const { statusCode, payload: responsePayload } = await server.inject({
+      method,
+      url,
+      payload,
+      headers: {
+        'x-signature': 'invalid-signature',
+        'x-timestamp': timestamp300SecondsAgo,
+        'x-signature-version': 'v1',
+        'content-type': 'application/json'
+      }
+    })
+
+    const { message } = JSON.parse(responsePayload)
+
+    expect(statusCode).toBe(401)
+    expect(message).toBe('Invalid signature')
+  })
+
+  test('call to trigger-test-suite passes with a valid signature', async () => {
+    const timestamp = Math.floor(Date.now() / 1000).toString()
+
+    const validSignature = generateSignature(
+      method,
+      url,
+      timestamp,
+      JSON.stringify(payload),
+      'mocked-secret'
+    )
+
+    runTestSuite.mockResolvedValue('mocked-run-id')
+
+    const { statusCode } = await server.inject({
+      method,
+      url,
+      payload,
+      headers: {
+        'x-signature': validSignature,
+        'x-timestamp': timestamp,
+        'x-signature-version': 'v1',
+        'content-type': 'application/json'
+      }
+    })
+
+    expect(statusCode).toBe(200)
+  })
+})
