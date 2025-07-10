74096c66043e1938cf838423d9e055699fe0abdf
chotai <chotai@users.noreply.github.com>
2025-07-10 10:25:56 +0100

    CORE-923 Add retry to commit file call to fix potential race conditions

--- src/helpers/github/commit-github-file.js
+++ src/helpers/github/commit-github-file.js
@@ -2,6 +2,34 @@ import { getLatestCommitSha } from '~/src/helpers/github/get-latest-commit-sha.j
 import { graphql } from '~/src/helpers/oktokit/oktokit.js'
 
 /**
+ *
+ * @template T
+ * @param {import('pino').Logger} logger
+ * @param {() => Promise<T>} fn
+ * @param {number} retries
+ * @param {number} delayMs
+ * @param {(error: any) => boolean} shouldRetry
+ * @returns {Promise<T>}
+ * @throws Will throw the last encountered error if retries are exhausted or shouldRetry returns false.
+ */
+async function retry(
+  logger,
+  fn,
+  retries = 3,
+  delayMs = 1000,
+  shouldRetry = () => true
+) {
+  try {
+    return await fn()
+  } catch (err) {
+    logger.error(err)
+    if (retries <= 0 || !shouldRetry(err)) throw err
+    await new Promise((resolve) => setTimeout(resolve, delayMs))
+    return retry(logger, fn, retries - 1, delayMs, shouldRetry)
+  }
+}
+
+/**
  * @returns {Promise<{object}>}
  * @param {string} owner
  * @param {string} repo
@@ -20,24 +48,28 @@ async function commitFile(
   content,
   logger
 ) {
-  let startTime = Date.now()
-  const commitSha = await getLatestCommitSha(owner, repo, branch)
-  logger.info(`getLatestCommitSha: ${commitSha} in ${Date.now() - startTime}ms`)
+  await retry(logger, async () => {
+    let startTime = Date.now()
+    const commitSha = await getLatestCommitSha(owner, repo, branch)
+    logger.info(
+      `getLatestCommitSha: ${commitSha} in ${Date.now() - startTime}ms`
+    )
 
-  startTime = Date.now()
-  const response = await createNewCommit(
-    owner,
-    repo,
-    branch,
-    commitSha,
-    commitMessage,
-    filePath,
-    content,
-    logger
-  )
-  logger.info(`createNewCommit: ${Date.now() - startTime}ms`)
+    startTime = Date.now()
+    const response = await createNewCommit(
+      owner,
+      repo,
+      branch,
+      commitSha,
+      commitMessage,
+      filePath,
+      content,
+      logger
+    )
+    logger.info(`createNewCommit: ${Date.now() - startTime}ms`)
 
-  return response
+    return response
+  })
 }
 
 async function createNewCommit(
@@ -87,4 +119,4 @@ async function createNewCommit(
   }
 }
 
-export { commitFile }
+export { commitFile, retry }
