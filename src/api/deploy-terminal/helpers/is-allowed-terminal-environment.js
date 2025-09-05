b30d6706b5d560c27b386c161b3d0ef05557422b
Ben Chidgey <ben@feedmypixel.com>
2025-09-05 12:13:52 +0100

    CORE-1156: Fix for the lower envs terminal

--- src/api/deploy-terminal/helpers/is-allowed-terminal-environment.js
+++ src/api/deploy-terminal/helpers/is-allowed-terminal-environment.js
@@ -4,25 +4,40 @@ import { environments } from '../../../config/index.js'
 
 function isAllowedTerminalEnvironment({ userScopes, environment, teamIds }) {
   const adminEnvs = [environments.infraDev, environments.management]
+  const lowerEnvs = [
+    environments.dev,
+    environments.test,
+    environments.perfTest,
+    environments.extTest
+  ]
   const userHasUserBreakGlass = userScopes.includes(scopes.breakGlass)
   const userHasTeamBasedBreakGlass = teamIds.some((teamId) =>
     userScopes.includes(`${scopes.breakGlass}:team:${teamId}`)
   )
+  const hasServiceOwnerBasedScope = teamIds.some((teamId) =>
+    userScopes.includes(`${scopes.serviceOwner}:team:${teamId}`)
+  )
 
+  // Prod env and user or member has break glass
   if (
-    (userHasUserBreakGlass || userHasTeamBasedBreakGlass) &&
-    environment === environments.prod
+    environment === environments.prod &&
+    (userHasUserBreakGlass || userHasTeamBasedBreakGlass)
   ) {
     return true
   }
 
-  // Non-admin can't run shells in admin environments.
-  if (adminEnvs.includes(environment) && !userScopes.includes(scopes.admin)) {
+  // Non-admin can't run shells in admin environments
+  if (!userScopes.includes(scopes.admin) && adminEnvs.includes(environment)) {
     return false
   }
 
-  // admin can run shells in admin environments.
-  if (adminEnvs.includes(environment) && userScopes.includes(scopes.admin)) {
+  // admin can run shells in admin environments
+  if (userScopes.includes(scopes.admin) && adminEnvs.includes(environment)) {
+    return true
+  }
+
+  // service owners can run shells in lower environments
+  if (lowerEnvs.includes(environment) && hasServiceOwnerBasedScope) {
     return true
   }
 
--- src/api/deploy-terminal/helpers/is-allowed-terminal-environment.test.js
+++ src/api/deploy-terminal/helpers/is-allowed-terminal-environment.test.js
@@ -24,6 +24,16 @@ describe('#isAllowedTerminalEnvironment', () => {
     expect(result).toBe(true)
   })
 
+  test('Should deny access to production without break glass scope', () => {
+    const result = isAllowedTerminalEnvironment({
+      userScopes: [],
+      environment: environments.prod,
+      teamIds: []
+    })
+
+    expect(result).toBe(false)
+  })
+
   test('Should deny access to admin environments without admin scope', () => {
     const result = isAllowedTerminalEnvironment({
       userScopes: [],
@@ -44,21 +54,39 @@ describe('#isAllowedTerminalEnvironment', () => {
     expect(result).toBe(true)
   })
 
-  test('Should deny access to production without break glass scope', () => {
+  test('Should deny access to admin environments with unrelated scopes', () => {
     const result = isAllowedTerminalEnvironment({
-      userScopes: [],
-      environment: environments.prod,
+      userScopes: [scopes.breakGlass],
+      environment: environments.management,
       teamIds: []
     })
 
     expect(result).toBe(false)
   })
 
-  test('Should deny access to admin environments with unrelated scopes', () => {
+  test('Should allow access to lower environments', () => {
     const result = isAllowedTerminalEnvironment({
-      userScopes: [scopes.breakGlass],
-      environment: environments.management,
-      teamIds: []
+      userScopes: [
+        'permission:serviceOwner:team:forms',
+        'team:forms',
+        'user:01b99595-27b5-4ab0-9807-f104c09d2cd0'
+      ],
+      environment: environments.dev,
+      teamIds: ['forms']
+    })
+
+    expect(result).toBe(true)
+  })
+
+  test('Should not allow access to lower environments for non service owner', () => {
+    const result = isAllowedTerminalEnvironment({
+      userScopes: [
+        'permission:serviceOwner:team:forms',
+        'team:forms',
+        'user:01b99595-27b5-4ab0-9807-f104c09d2cd0'
+      ],
+      environment: environments.dev,
+      teamIds: ['platform']
     })
 
     expect(result).toBe(false)
