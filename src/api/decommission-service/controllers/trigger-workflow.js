451c1b54e0a75cfd0e1416623289784193eda839
Phil Segal <phil@phisec.co.uk>
2025-10-22 15:48:07 +0100

    CORE-1584: Rename endpoint & parameter

--- src/api/decommission-service/controllers/trigger-workflow.js
+++ src/api/decommission-service/controllers/trigger-workflow.js
@@ -4,33 +4,33 @@ import {
   statusCodes
 } from '@defra/cdp-validation-kit'
 
-import { triggerRemoveWorkflows } from '../helpers/trigger-remove-workflows.js'
+import { triggerRemoveWorkflow } from '../helpers/trigger-remove-workflow.js'
 import { getEntity } from '../../../helpers/portal-backend/get-entity.js'
 import { validatePortalBackendRequest } from '../../helpers/pre/validate-portal-backend-request.js'
 
-const decommissionTriggerWorkflowsController = {
+const decommissionTriggerWorkflowController = {
   options: {
     pre: [validatePortalBackendRequest],
     validate: {
       params: Joi.object({
-        serviceName: repositoryNameValidation
+        entityName: repositoryNameValidation
       })
     }
   },
   handler: async (request, h) => {
-    const serviceName = request.params.serviceName
+    const entityName = request.params.entityName
     const logger = request.logger
 
-    const entity = await getEntity(serviceName, logger)
+    const entity = await getEntity(entityName, logger)
 
-    await triggerRemoveWorkflows(serviceName, entity, logger)
+    await triggerRemoveWorkflow(entity, logger)
 
     return h
       .response({
-        message: `Decommission workflows have been triggered for: ${serviceName}`
+        message: `Decommission workflow has been triggered for: ${entityName}`
       })
       .code(statusCodes.ok)
   }
 }
 
-export { decommissionTriggerWorkflowsController }
+export { decommissionTriggerWorkflowController }
---
+++ src/api/decommission-service/helpers/trigger-remove-workflow.test.js
@@ -0,0 +1,74 @@
+import { triggerRemoveTenantWorkflow } from '../../../helpers/remove/workflows/trigger-remove-tenant-workflow.js'
+
+vi.mock('../../../helpers/oktokit/oktokit.js', () => ({
+  octokit: vi.fn(),
+  graphql: vi.fn(),
+  request: vi.fn()
+}))
+
+vi.mock('../../../helpers/remove/workflows/trigger-remove-tenant-workflow.js')
+
+const logger = {
+  info: vi.fn()
+}
+
+describe('#triggerRemoveWorkflow', () => {
+  const serviceName = 'some-service'
+  const backendEntity = {
+    name: serviceName,
+    type: 'Microservice',
+    subType: 'Backend'
+  }
+  const frontendEntity = {
+    name: serviceName,
+    type: 'Microservice',
+    subType: 'Frontend'
+  }
+  const testEntity = {
+    name: serviceName,
+    type: 'TestSuite',
+    subType: 'Journey'
+  }
+
+  test('Should trigger relevant workflow when run for backend repo', async () => {
+    const { triggerRemoveWorkflow } = await import(
+      './trigger-remove-workflow.js'
+    )
+
+    await triggerRemoveWorkflow(backendEntity, logger)
+
+    expect(triggerRemoveTenantWorkflow).toHaveBeenCalledWith(
+      serviceName,
+      'Microservice',
+      logger
+    )
+  })
+
+  test('Should trigger relevant workflow when run for frontend repo', async () => {
+    const { triggerRemoveWorkflow } = await import(
+      './trigger-remove-workflow.js'
+    )
+
+    await triggerRemoveWorkflow(frontendEntity, logger)
+
+    expect(triggerRemoveTenantWorkflow).toHaveBeenCalledWith(
+      serviceName,
+      'Microservice',
+      logger
+    )
+  })
+
+  test('Should trigger relevant workflow when run for test suite', async () => {
+    const { triggerRemoveWorkflow } = await import(
+      './trigger-remove-workflow.js'
+    )
+
+    await triggerRemoveWorkflow(testEntity, logger)
+
+    expect(triggerRemoveTenantWorkflow).toHaveBeenCalledWith(
+      serviceName,
+      'TestSuite',
+      logger
+    )
+  })
+})
