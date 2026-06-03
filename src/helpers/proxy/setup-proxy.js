92897334d8c551a338956026c0184d185167a0f9
chotai <chotai@users.noreply.github.com>
2026-06-03 15:36:38 +0100

    CORE-2865 Use Node proxy env

--- src/helpers/proxy/setup-proxy.js
+++
@@ -1,9 +0,0 @@
-import { createLogger } from '../logging/logger.js'
-
-const logger = createLogger()
-
-export function setupProxy() {
-  if (process.env.HTTPS_PROXY) {
-    logger.info('Routing outbound requests via proxy')
-  }
-}
