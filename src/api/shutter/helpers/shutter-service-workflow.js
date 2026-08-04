5bc4cee07d95edac6f1c32c4f85fc2279f43e5df
Lukasz Tymoszczuk <publicperson@gmail.com>
2026-08-04 10:36:28 +0100

    CORE-3294: Clean up v1 shutter (#633)

--- src/api/shutter/helpers/shutter-service-workflow.js
+++ src/api/shutter/helpers/shutter-service-workflow.js
@@ -1,15 +1,6 @@
 import { config } from '#config/config.js'
-import { triggerWorkflow } from '../../../helpers/github/trigger-workflow.js'
 import { registerShuttering } from './register-shuttering.js'
 import { sendSnsMessage } from '../../../helpers/sns/send-sns-message.js'
-import { shouldUseShutterV2 } from './should-use-shutter-v2.js'
-
-const buildWorkFlowInputs = (inputs) => ({
-  service_name: inputs.serviceName,
-  environment: inputs.environment,
-  url: inputs.url,
-  url_type: inputs.urlType
-})
 
 async function publishManageShutteringEvent(inputs, action, snsClient, logger) {
   const topic = config.get('monoLambdaTriggerTopicArn')
@@ -36,43 +27,13 @@ async function publishManageShutteringEvent(inputs, action, snsClient, logger) {
 }
 
 async function shutterServiceWorkflow(inputs, user, logger, snsClient) {
-  const org = config.get('github.org')
-  const repo = config.get('github.repos.cdpTenantConfig')
-  const workflow = config.get('workflows.addShutterWorkflow')
-
-  if (shouldUseShutterV2(inputs.environment)) {
-    await publishManageShutteringEvent(inputs, 'shutter', snsClient, logger)
-  } else {
-    await triggerWorkflow(
-      org,
-      repo,
-      workflow,
-      buildWorkFlowInputs(inputs),
-      inputs.url,
-      logger
-    )
-  }
+  await publishManageShutteringEvent(inputs, 'shutter', snsClient, logger)
 
   await registerShuttering({ ...inputs, shuttered: true, actionedBy: user })
 }
 
 async function unshutterServiceWorkflow(inputs, user, logger, snsClient) {
-  const org = config.get('github.org')
-  const repo = config.get('github.repos.cdpTenantConfig')
-  const workflow = config.get('workflows.removeShutterWorkflow')
-
-  if (shouldUseShutterV2(inputs.environment)) {
-    await publishManageShutteringEvent(inputs, 'unshutter', snsClient, logger)
-  } else {
-    await triggerWorkflow(
-      org,
-      repo,
-      workflow,
-      buildWorkFlowInputs(inputs),
-      inputs.url,
-      logger
-    )
-  }
+  await publishManageShutteringEvent(inputs, 'unshutter', snsClient, logger)
 
   await registerShuttering({ ...inputs, shuttered: false, actionedBy: user })
 }
