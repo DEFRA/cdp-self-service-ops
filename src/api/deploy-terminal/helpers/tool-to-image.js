7fdb181e636d45f7c2940a4f2fea357e79f48a84
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-08-27 15:34:41 +0100

    extends tools selection

--- src/api/deploy-terminal/helpers/tool-to-image.js
+++ src/api/deploy-terminal/helpers/tool-to-image.js
@@ -7,7 +7,15 @@ export const toolToImage = {
     image: 'cdp-webshell',
     image_version: 'stable'
   },
+  terminal_latest: {
+    image: 'cdp-webshell',
+    image_version: 'latest'
+  },
   pgweb: {
+    image: 'cdp-pgweb',
+    image_version: 'stable'
+  },
+  pgweb_latest: {
     image: 'cdp-pgweb',
     image_version: 'latest'
   }
